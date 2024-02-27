170a6d17c15a92bf4489976952ef4d9c48ec447c
peixed351 <peixed351@gmail.com>
2024-02-27 11:38:00 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "NDD20"
 PROJECT_NAME_FULL: str = "NDD20: The Northumberland Dolphin Dataset 2020"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
